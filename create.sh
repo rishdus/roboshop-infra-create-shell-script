dcb11dc6a1793f4edd7948ee43c9391880db4f5b
rishdus <risheek.dustin@gmail.com>
2022-08-21 19:51:07 +0530

    updating ./roboshop-infra-create-shell-script

--- create.sh
+++ create.sh
@@ -21,7 +21,8 @@ create_ec2() {
   aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
 }
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+#AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
+AMI_ID=ami-04e154a5181609807
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 
