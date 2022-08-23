97a9fbd92b3037b72b614cec34041761e20520df
rishdus <risheek.dustin@gmail.com>
2022-08-23 11:06:24 +0530

    updating ./roboshop-infra-create-shell-script

--- create.sh
+++ create.sh
@@ -22,7 +22,7 @@ create_ec2() {
 }
 
 #AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
-AMI_ID=ami-04e154a5181609807
+AMI_ID=ami-0f0d5d950bc2520f0
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NAME} | jq  '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
 
